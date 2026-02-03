15ad2ae3e810b45c07732d0e8f642af6ab4e462b
Olaitan Soyoye <soyoyeolaitan@yahoo.com>
2026-02-03 01:28:59 +0000

    Add outputs for AWS resources in outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,20 @@
+output "s3_bucket_name" {
+  value = aws_s3_bucket.frontend_bucket.bucket
+}
+
+output "cloudfront_id" {
+  value = aws_cloudfront_distribution.frontend_cdn.id
+}
+
+output "cognito_pool_id" {
+  value = aws_cognito_user_pool.main_pool.id
+}
+
+output "api_gateway_id" {
+  value = aws_api_gateway_rest_api.family_api.id
+}
+
+output "lambda_upload_name" {
+  value = aws_lambda_function.upload.function_name
+}
 
