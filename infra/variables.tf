2c97109bdabbee27370678cc7d581189deaaa4e3
Olaitan Soyoye <soyoyeolaitan@yahoo.com>
2026-02-03 01:28:37 +0000

    Define variables for AWS resources in variables.tf
    Added variables for AWS region, S3 bucket, Cognito user pool, and API Gateway.

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,21 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the existing S3 bucket"
+  type        = string
+}
+
+variable "cognito_pool_name" {
+  description = "Name of the Cognito user pool"
+  type        = string
+}
+
+variable "api_name" {
+  description = "Name of the API Gateway"
+  type        = string
+}
 
